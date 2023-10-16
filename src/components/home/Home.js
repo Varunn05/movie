2684088724f8939ab7ae2ec28e8ff13f000a25c8
Vaun <varunmarothiyaa@gmail.com>
2023-10-17 00:19:36 +0530

    first commit

---
+++ src/components/home/Home.js
@@ -0,0 +1,9 @@
+import Hero from '../actor/Actor';
+
+const Home = ({movies}) => {
+  return (
+    <Hero movies = {movies} />
+  )
+}
+
+export default Home
---
+++ src/components/notFound/NotFound.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NotFound = () => {
+    <div>
+        This page could not be found
+    </div>
+}
+
+export default NotFound
